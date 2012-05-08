add24db60a1f0a5d6b11a778cdb934ab62fc4d57
Merouane Atig <yahoocbien-dev@yahoo.fr>
2012-05-08 23:18:14 +0200

    add list and save method

--- lib/ardour_git.rb
+++ lib/ardour_git.rb
@@ -2,8 +2,17 @@ require 'git'
 
 class ArdourGit
   def create
-    g = Git.init('.',
+    @g = Git.init('.',
                  { :repository => '.ardourgit',
                    :index => '.ardourgit/index'})
+  end
+
+  def save
+    files = Dir.glob('*.ardour')
+    @g.add files
+  end
+
+  def list
+    @g.ls_files.keys
   end
 end
--- spec/ardour_git_spec.rb
+++ spec/ardour_git_spec.rb
@@ -1,12 +1,40 @@
 require_relative '../lib/ardour_git'
 
 describe ArdourGit do
-  after(:each) do
-    FileUtils.rm_rf('.ardourgit')
+  before(:each) do
   end
 
-  it "creates a git repository when create command is called" do
-    ArdourGit.new.create
-    (FileTest.exist? ".ardourgit").should == true
+  after(:each) do
+    FileUtils.rm_rf('tmp')
+  end
+
+  it 'creates a git repository when create command is called' do
+    FileUtils.mkdir('tmp')
+    Dir.chdir('tmp') do
+      ArdourGit.new.create
+      (FileTest.exist? '.ardourgit').should == true
+    end
+  end
+
+  it 'adds and commits session file when save command is called' do
+    FileUtils.mkdir('tmp')
+    Dir.chdir('tmp') do
+      repository = ArdourGit.new
+      repository.create
+      FileUtils.touch 'something.ardour'
+      repository.save
+      repository.list.should == ['something.ardour']
+    end
+  end
+
+  it 'does not add file which is not an ardour session' do
+    FileUtils.mkdir('tmp')
+    Dir.chdir('tmp') do
+      repository = ArdourGit.new
+      repository.create
+      FileUtils.touch 'something.txt'
+      repository.save
+      repository.list.should == []
+    end
   end
 end
