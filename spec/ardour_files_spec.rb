d603f8f98cb95baa6b7993b80746a724078b0c23
Merouane Atig <yahoocbien-dev@yahoo.fr>
2012-05-10 22:01:37 +0200

    chdir in before/after

--- spec/ardour_files_spec.rb
+++ spec/ardour_files_spec.rb
@@ -3,19 +3,19 @@ require_relative '../lib/ardour_files'
 describe ArdourFiles do
   before(:each) do
     FileUtils.mkdir('tmp')
+    Dir.chdir('tmp')
   end
 
   after(:each) do
+    Dir.chdir('..')
     FileUtils.rm_rf('tmp')
   end
 
   it 'returns an empty file list when no files exist'
 
   it 'returns the ardour session file when it exists' do
-    Dir.chdir('tmp') do
-      FileUtils.touch('session.ardour')
-      ArdourFiles.list.should == ['session.ardour']
-    end
+    FileUtils.touch('session.ardour')
+    ArdourFiles.list.should == ['session.ardour']
   end
 
   it 'parses the ardour session file to return all project files'
