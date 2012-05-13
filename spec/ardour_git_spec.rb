456482ad0aa6741428d0a31124c316529d8175db
Merouane Atig <yahoocbien-dev@yahoo.fr>
2012-05-13 22:32:46 +0200

    work in tmp dir for each test

--- spec/ardour_git_spec.rb
+++ spec/ardour_git_spec.rb
@@ -2,39 +2,33 @@ require_relative '../lib/ardour_git'
 
 describe ArdourGit do
   before(:each) do
+    FileUtils.mkdir('tmp')
+    Dir.chdir('tmp')
   end
 
   after(:each) do
+    Dir.chdir('..')
     FileUtils.rm_rf('tmp')
   end
 
   it 'creates a git repository when create command is called' do
-    FileUtils.mkdir('tmp')
-    Dir.chdir('tmp') do
-      ArdourGit.new.create
-      (FileTest.exist? '.ardourgit').should == true
-    end
+    ArdourGit.new.create
+    (FileTest.exist? '.ardourgit').should == true
   end
 
-  it 'adds and commits session file when save command is called' do
-    FileUtils.mkdir('tmp')
-    Dir.chdir('tmp') do
-      repository = ArdourGit.new
-      repository.create
-      FileUtils.touch 'something.ardour'
-      repository.save
-      repository.list.should == ['something.ardour']
-    end
+  it 'adds session file when save command is called' do
+    repository = ArdourGit.new
+    repository.create
+    FileUtils.touch 'something.ardour'
+    repository.save
+    repository.list.should == ['something.ardour']
   end
 
   it 'does not add file which is not an ardour session' do
-    FileUtils.mkdir('tmp')
-    Dir.chdir('tmp') do
-      repository = ArdourGit.new
-      repository.create
-      FileUtils.touch 'something.txt'
-      repository.save
-      repository.list.should == []
-    end
+    repository = ArdourGit.new
+    repository.create
+    FileUtils.touch 'something.txt'
+    repository.save
+    repository.list.should == []
   end
 end
