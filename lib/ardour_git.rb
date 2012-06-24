a1c7314507cbc95d87617aba306c102aac308a8e
Merouane Atig <yahoocbien-dev@yahoo.fr>
2012-06-24 15:32:22 +0200

    parse commands in ArdourGit

--- lib/ardour_git.rb
+++ lib/ardour_git.rb
@@ -2,6 +2,14 @@ require_relative 'ardour_files'
 require_relative 'git_repository'
 
 class ArdourGit
+  def command(opts)
+    if opts[:init]
+      create
+    elsif opts[:save]
+      save
+    end
+  end
+
   def create
     GitRepository.create
   end
