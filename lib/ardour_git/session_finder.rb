fa9f2d6d77ba7057a62b0bda58e60013f5cf785a
Merouane Atig <yahoocbien-dev@yahoo.fr>
2012-07-08 22:42:08 +0200

    change signature of SessionFinder

--- lib/ardour_git/session_finder.rb
+++ lib/ardour_git/session_finder.rb
@@ -1,5 +1,11 @@
 class SessionFinder
-  def self.list
-    Dir.glob('*.ardour')
+  def self.file
+    session_files = Dir.glob("*.ardour")
+    raise NoSessionFound if session_files.empty?
+    raise TooManySessions if session_files.count > 1
+    session_files.first
   end
+
+  class NoSessionFound < Exception; end
+  class TooManySessions < Exception; end
 end
--- spec/session_finder_spec.rb
+++ spec/session_finder_spec.rb
@@ -11,14 +11,21 @@ describe SessionFinder do
     FileUtils.rm_rf('tmp')
   end
 
-  it 'returns an empty list when no files exist' do
-    SessionFinder.list.should == []
+  it 'raises NoSessionFound when no ardour session file exist' do
+    FileUtils.touch('session2')
+    expect { SessionFinder.file }.to raise_error(SessionFinder::NoSessionFound)
   end
 
-  it 'finds all session files in the project' do
+  it 'finds the ardour session file in current directory' do
+    FileUtils.touch('session1.ardour')
+    FileUtils.touch('session2')
+    SessionFinder.file.should == 'session1.ardour'
+  end
+
+  it 'throws an exception when two ardour session files are found' do
     FileUtils.touch('session1.ardour')
     FileUtils.touch('session2')
     FileUtils.touch('session3.ardour')
-    SessionFinder.list.should == ['session1.ardour', 'session3.ardour']
+    expect { SessionFinder.file }.to raise_error(SessionFinder::TooManySessions)
   end
 end
