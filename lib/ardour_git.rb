328416223245b505ba6bc2eb2fae87f835fd85b1
Merouane Atig <yahoocbien-dev@yahoo.fr>
2012-07-12 00:24:09 +0200

    issue #15: add an optional message to save command

--- lib/ardour_git.rb
+++ lib/ardour_git.rb
@@ -2,11 +2,16 @@ require_relative 'ardour_git/ardour_files'
 require_relative 'ardour_git/git_repository'
 
 class ArdourGit
-  def command(opts)
+  def command(opts, argv)
     if opts[:init]
       create
     elsif opts[:save]
-      save
+      if argv.empty?
+        message = "test"
+      else
+        message = argv[0]
+      end
+      save(message)
     end
   end
 
@@ -14,9 +19,9 @@ class ArdourGit
     GitRepository.create
   end
 
-  def save
+  def save(message)
     files = ArdourFiles.list
     GitRepository.add(files)
-    GitRepository.commit('test')
+    GitRepository.commit(message)
   end
 end
