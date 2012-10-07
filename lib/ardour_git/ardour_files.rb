2ec1470fe836aff4bafa8bfab0917a4339a81c42
Merouane Atig <yahoocbien-dev@yahoo.fr>
2012-10-07 23:14:47 +0200

    implement list_audio

--- lib/ardour_git/ardour_files.rb
+++ lib/ardour_git/ardour_files.rb
@@ -5,10 +5,16 @@ class ArdourFiles
   def self.list
     begin
       session_file = SessionFinder.file
-      files = [session_file]
+      files = [session_file, 'instant.xml', '.history']
+    rescue SessionFinder::NoSessionFound
+      []
+    end
+  end
+
+  def self.list_audio
+    begin
+      session_file = SessionFinder.file
       audio_files = SessionParser.list_audio_files(session_file)
-      files += audio_files
-      files += ['instant.xml', '.history']
     rescue SessionFinder::NoSessionFound
       []
     end
